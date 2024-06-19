5167195d35b5aa16e7ad9661edc26a8354382c40
victor_prof@yahoo.com <gb74#hb@14>
2024-06-19 02:33:43 -0700

    Adding services, types and utils

---
+++ src/utils/constants/text-consts.ts
@@ -0,0 +1,8 @@
+export enum DefaultContent {
+  errorMessageLine1 = "Apologies, an error occurred while retrieving the data.",
+  errorMessageLine2 = "Please try again later.",
+  headerTitle = "Wikipedia: Born On This Day",
+  headerText = "Births of notable people that occurred in history on",
+  fetchButton = "Click to load birthdays",
+  loadingData = "Loading Data...",
+}
